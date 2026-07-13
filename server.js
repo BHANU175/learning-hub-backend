d0aefeef6374118be2ce9463eb1a7f2d2db6d60b
ashish-7561 <ashishk56964@gmail.com>
2026-07-13 16:38:46 +0530

    d

--- server.js
+++ server.js
@@ -68,3 +68,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`📡 Server broadcasting on port ${PORT}`);
 });
+
+// EXPORT FOR VERCEL (Must be outside the app.listen block)
+module.exports = app;
